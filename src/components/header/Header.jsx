e6e14eda6af303806b19f2ea26bb6c350894bb68
artemriabyi6 <ar04082001@gmail,com>
2025-05-05 22:14:32 +0300

    sticky header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,46 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { images } from '../../utils/images';
 import styles from './header.module.scss';
 import NavItem from '../navItem/NavItem';
 
 const Header = () => {
-    return ( 
-        <header className={styles.header}>
-            <div className={`container ${styles.container}`}>
-                <div className={styles.logoBlock}>
-                    <img src={images.icons.logo} alt="logo" />
-                </div>
-                <div className={styles.otherBlock}>
-                    <nav className={styles.navigation}>
-                        <ul>
-                            <li>
-                                <Link to='/сети' className={styles.menuItem}>Меню</Link>
-                            </li>
-                            <li>
-                                <Link to='/акції' className={`${styles.menuItem} ${styles.specialItem}`}>Акції</Link>
-                            </li>
-                            <li>
-                                <Link to='/delievery' className={styles.menuItem}>Доставка/Оплата</Link>
-                            </li>
-                            <li>
-                                <Link to='/contact' className={styles.menuItem}>Контакти</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div className={styles.adressBlock}>
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 150);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <>
+            <div className={isScrolled ? styles.placeholder : ''}></div>
+            <header className={`${styles.header} ${isScrolled ? styles.scrolled : ''}`}>
+                <div className={`container ${styles.container}`}>
+                    <div className={styles.logoBlock}>
+                        <img src={images.icons.logo} alt="logo" />
+                    </div>
+                    <div className={styles.otherBlock}>
+                        <nav className={styles.navigation}>
+                            <ul>
+                                <li>
+                                    <Link to='/сети' className={styles.menuItem}>Меню</Link>
+                                </li>
+                                <li>
+                                    <Link to='/акції' className={`${styles.menuItem} ${styles.specialItem}`}>Акції</Link>
+                                </li>
+                                <li>
+                                    <Link to='/delievery' className={styles.menuItem}>Доставка/Оплата</Link>
+                                </li>
+                                <li>
+                                    <Link to='/contact' className={styles.menuItem}>Контакти</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className={styles.adressBlock}>
                         <div className={styles.adressBlockFirstRow}>
                             <div className={styles.adressBlockFirstRowIconBox}>
                                 <img src={images.icons.locationPin} alt="Geolocation" />
@@ -46,24 +59,21 @@ const Header = () => {
                         </div>
                         <p className={styles.contactBlockSecondRowText}>з 10-30 до 21-00</p>
                     </div>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.stickyNav}>
-            <div className={`container ${styles.container}`}>
+                <div className={`container ${styles.container}`}>
                     <NavItem icon={images.icons.roll} text='Роли' />
                     <NavItem icon={images.icons.set} text='Сети' />
                     <NavItem icon={images.icons.geisha} text="З любов'ю від шефа" />
                     <NavItem icon={images.icons.can} text='Напої' />
                     <NavItem icon={images.icons.pizza} text='Піца' />
                     <NavItem icon={images.icons.sale} text='Акції' />
-            </div>
-            </div>
-            
-        </header>
+                </div>
+            </header>
+        </>
     );
-}
+};
 
 export default Header;
-
 
 
