c5009fa1484cfaae4db105728b502ab2506cc452
artemriabyi6 <ar04082001@gmail,com>
2025-05-11 17:08:55 +0300

    changed favicon

--- src/pages/menuItem/MenuItem.jsx
+++ src/pages/menuItem/MenuItem.jsx
@@ -289,7 +289,7 @@ const MenuCategoryPage = () => {
     <>
       {isMobile ? (
         <>
-         <Header />
+            <Header />
           <SubHeader />
         {
         menuItem !== 'special' ?  <MainSlider /> : null
